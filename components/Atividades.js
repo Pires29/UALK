1407d252f9518fd98d1452a17a9ae1018bd42b32
duarte99889 <duarte00@icloud.com>
2024-05-14 23:39:07 +0100

    atividades

---
+++ components/Atividades.js
@@ -0,0 +1,200 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+const AtividadesItem = () => {
+    return (
+        <View>
+            <View style={styles.container}>
+                <Image
+                    source={{ uri: 'https://via.placeholder.com/150' }}
+                    style={styles.imagem}
+                />
+                <View style={styles.detalhes}>
+                    <Text style={styles.nome}>Atividade 1</Text>
+                    <Text style={styles.classificacao}>Classificação: 4.5</Text>
+                    <Text style={styles.descricao}>
+                        Este é um percurso agradável que se inicia na Casa do Estudante e
+                        acaba na belíssima marinha da Casqueira.
+                    </Text>
+                </View>
+            </View>
+            <View style={styles.container}>
+                <Image
+                    source={{ uri: 'https://via.placeholder.com/150' }}
+                    style={styles.imagem}
+                />
+                <View style={styles.detalhes}>
+                    <Text style={styles.nome}>Atividade 2</Text>
+                    <Text style={styles.classificacao}>Classificação: 4.5</Text>
+                    <Text style={styles.descricao}>
+                        Este é um percurso agradável que se inicia na Casa do Estudante e
+                        acaba na belíssima marinha da Casqueira.
+                    </Text>
+                </View>
+            </View>
+            <View style={styles.container}>
+                <Image
+                    source={{ uri: 'https://via.placeholder.com/150' }}
+                    style={styles.imagem}
+                />
+                <View style={styles.detalhes}>
+                    <Text style={styles.nome}>Atividade 2</Text>
+                    <Text style={styles.classificacao}>Classificação: 4.5</Text>
+                    <Text style={styles.descricao}>
+                        Este é um percurso agradável que se inicia na Casa do Estudante e
+                        acaba na belíssima marinha da Casqueira.
+                    </Text>
+                </View>
+            </View>
+            <View style={styles.container}>
+                <Image
+                    source={{ uri: 'https://via.placeholder.com/150' }}
+                    style={styles.imagem}
+                />
+                <View style={styles.detalhes}>
+                    <Text style={styles.nome}>Atividade 2</Text>
+                    <Text style={styles.classificacao}>Classificação: 4.5</Text>
+                    <Text style={styles.descricao}>
+                        Este é um percurso agradável que se inicia na Casa do Estudante e
+                        acaba na belíssima marinha da Casqueira.
+                    </Text>
+                </View>
+            </View>
+            <View style={styles.container}>
+                <Image
+                    source={{ uri: 'https://via.placeholder.com/150' }}
+                    style={styles.imagem}
+                />
+                <View style={styles.detalhes}>
+                    <Text style={styles.nome}>Atividade 2</Text>
+                    <Text style={styles.classificacao}>Classificação: 4.5</Text>
+                    <Text style={styles.descricao}>
+                        Este é um percurso agradável que se inicia na Casa do Estudante e
+                        acaba na belíssima marinha da Casqueira.
+                    </Text>
+                </View>
+            </View>
+            <View style={styles.container}>
+                <Image
+                    source={{ uri: 'https://via.placeholder.com/150' }}
+                    style={styles.imagem}
+                />
+                <View style={styles.detalhes}>
+                    <Text style={styles.nome}>Atividade 2</Text>
+                    <Text style={styles.classificacao}>Classificação: 4.5</Text>
+                    <Text style={styles.descricao}>
+                        Este é um percurso agradável que se inicia na Casa do Estudante e
+                        acaba na belíssima marinha da Casqueira.
+                    </Text>
+                </View>
+            </View>
+            <View style={styles.container}>
+                <Image
+                    source={{ uri: 'https://via.placeholder.com/150' }}
+                    style={styles.imagem}
+                />
+                <View style={styles.detalhes}>
+                    <Text style={styles.nome}>Atividade 2</Text>
+                    <Text style={styles.classificacao}>Classificação: 4.5</Text>
+                    <Text style={styles.descricao}>
+                        Este é um percurso agradável que se inicia na Casa do Estudante e
+                        acaba na belíssima marinha da Casqueira.
+                    </Text>
+                </View>
+            </View>
+            <View style={styles.container}>
+                <Image
+                    source={{ uri: 'https://via.placeholder.com/150' }}
+                    style={styles.imagem}
+                />
+                <View style={styles.detalhes}>
+                    <Text style={styles.nome}>Atividade 2</Text>
+                    <Text style={styles.classificacao}>Classificação: 4.5</Text>
+                    <Text style={styles.descricao}>
+                        Este é um percurso agradável que se inicia na Casa do Estudante e
+                        acaba na belíssima marinha da Casqueira.
+                    </Text>
+                </View>
+            </View>
+            <View style={styles.container}>
+                <Image
+                    source={{ uri: 'https://via.placeholder.com/150' }}
+                    style={styles.imagem}
+                />
+                <View style={styles.detalhes}>
+                    <Text style={styles.nome}>Atividade 2</Text>
+                    <Text style={styles.classificacao}>Classificação: 4.5</Text>
+                    <Text style={styles.descricao}>
+                        Este é um percurso agradável que se inicia na Casa do Estudante e
+                        acaba na belíssima marinha da Casqueira.
+                    </Text>
+                </View>
+            </View>
+            <View style={styles.container}>
+                <Image
+                    source={{ uri: 'https://via.placeholder.com/150' }}
+                    style={styles.imagem}
+                />
+                <View style={styles.detalhes}>
+                    <Text style={styles.nome}>Atividade 2</Text>
+                    <Text style={styles.classificacao}>Classificação: 4.5</Text>
+                    <Text style={styles.descricao}>
+                        Este é um percurso agradável que se inicia na Casa do Estudante e
+                        acaba na belíssima marinha da Casqueira.
+                    </Text>
+                </View>
+            </View>
+            <View style={styles.container}>
+                <Image
+                    source={{ uri: 'https://via.placeholder.com/150' }}
+                    style={styles.imagem}
+                />
+                <View style={styles.detalhes}>
+                    <Text style={styles.nome}>Atividade 2</Text>
+                    <Text style={styles.classificacao}>Classificação: 4.5</Text>
+                    <Text style={styles.descricao}>
+                        Este é um percurso agradável que se inicia na Casa do Estudante e
+                        acaba na belíssima marinha da Casqueira.
+                    </Text>
+                </View>
+            </View>
+        </View>
+
+
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        borderBottomWidth: 3,
+        backgroundColor: '#2C333C',
+        borderBottomColor: '#62BB76',
+    },
+    imagem: {
+        width: 100,
+        height: 100,
+        borderRadius: 8,
+        marginRight: 10,
+    },
+    detalhes: {
+        flex: 1,
+    },
+    nome: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    classificacao: {
+        color: 'white',
+        marginBottom: 5,
+    },
+    descricao: {
+        color: 'white',
+    },
+});
+
+export default AtividadesItem;
