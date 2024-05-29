5201b32a2ffca91b84caa7bcb1a07be519de5a24
Pires29 <renatopiress29@gmail.com>
2024-05-29 01:11:50 +0100

    Teste

--- App.js
+++ App.js
@@ -2,25 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import NavBar from './components/NavBar';
-<<<<<<< Celina
 import Mapa from './pages/Map'
 import  DescriptionPage from './pages/Description'
 import PercursoFlowConst from '../UALK/pages/PercursoFlow'
 import PaginaAvaliacao from "./pages/PaginaAvaliacao";
 import Media from "./components/media";
-
-export default function App() {
-  return (
-      <PaginaAvaliacao/>
-=======
 import Favorites from './pages/Favorites'; // Importe o componente Favorites aqui
 import PopUp from './components/PopUp';
 
 export default function App() {
   return (
       <NavBar/>
-      /*<PopUp/>*/
->>>>>>> MergeTeste
+      /*<PopUp/>
+      <PaginaAvaliacao/>
+      */
+
   );
 }
 
